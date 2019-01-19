e771a81580a80f1a07a1d07eded38295be6c4f91
grodfor <vlad.sitsko@gmail.com>
2019-01-19 22:51:35 +0300

    Basic interface

--- app/src/main/java/dracula/vlad/androcalc/MainActivity.java
+++ app/src/main/java/dracula/vlad/androcalc/MainActivity.java
@@ -1,13 +1,68 @@
 package dracula.vlad.androcalc;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editText);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            editText.setShowSoftInputOnFocus(false);
+        }
+        else{
+            editText.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    v.onTouchEvent(event);
+                    InputMethodManager inputMethod = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (inputMethod != null) {
+                        inputMethod.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    }
+                    return true;
+                }
+            });
+        }
+
+        getChildViews((ViewGroup) findViewById(R.id.ll_nums));
+
+    }
+
+    static void getChildViews(ViewGroup viewGroup) {
+        if (viewGroup != null) {
+            int children = viewGroup.getChildCount();
+            for (int i = 0; i < children; i++) {
+                View view = viewGroup.getChildAt(i);
+                if (view instanceof ViewGroup) {
+                    getChildViews((ViewGroup) view);
+                }
+                if (view instanceof Button) {
+                    ((Button) view).setTextColor(Color.BLUE);
+                }
+            }
+        }
     }
 }
