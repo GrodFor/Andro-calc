9c3e63068895b6a8b624d92c429e015be6a451f5
grodfor <vlad.sitsko@gmail.com>
2019-03-04 03:01:58 +0300

    begin refactoring

--- app/src/main/java/dracula/vlad/androcalc/MainActivity.java
+++ app/src/main/java/dracula/vlad/androcalc/MainActivity.java
@@ -27,13 +27,18 @@ import static dracula.vlad.androcalc.ShuntingYard.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String TAG = "LOG_Main";
-    private String input = "";
-    private ArrayList<Button> buttonArrayList= new ArrayList<>();
-    private double result;
-    TextView tv_Res=null;
+    private static final String TAG = MainActivity.class.getName();
 
-    @SuppressLint("ClickableViewAccessibility")
+    private String userInput = "";
+    private ArrayList<Button> buttons = new ArrayList<>();
+    private double userResult;
+
+    private TextView resultView = null;
+    private EditText inputView;
+    private Button clearAllButton;
+    private Button deleteOneButton;
+    private Button dotButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,67 +46,45 @@ public class MainActivity extends AppCompatActivity {
 
         getChildViews(findViewById(R.id.ll_Buttons));
 
-        final EditText editText = findViewById(R.id.et_Input);
-        editText.requestFocus();
+        inputView = findViewById(R.id.et_Input);
+        inputView.requestFocus();
 
-        tv_Res = findViewById(R.id.tv_Result);
+        resultView = findViewById(R.id.tv_Result);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            editText.setShowSoftInputOnFocus(false);
-        }
-        else{
-            editText.setOnTouchListener((v, event) -> {
-                v.onTouchEvent(event);
-                InputMethodManager inputMethod = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                if (inputMethod != null) {
-                    inputMethod.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                }
-                return true;
-            });
-        }
+        hideSoftwareKeyboard();
 
-        editText.addTextChangedListener(new TextWatcher() {
+        inputView.addTextChangedListener(new TextWatcher() {
 
-            // the user's changes are saved here
             public void onTextChanged(CharSequence c, int start, int before, int count) {
-                input=c.toString();
-                Log.d(TAG, "1 onTextChanged input: "+input);
+                userInput = c.toString();
+                Log.d(TAG, "1 onTextChanged input: " + userInput);
 
-                input=input.replaceAll("([+\\-/*])(\\d)"," $1 $2");
-                Log.d(TAG, "2 onTextChanged input: "+input);
+                userInput = userInput.replaceAll("([+\\-/*])(\\d)", " $1 $2");
+                Log.d(TAG, "2 onTextChanged input: " + userInput);
 
-                input=input.trim().replaceAll("(^-)(\\s)","$1");
-                Log.d(TAG, "3 onTextChanged input: "+input);
+                userInput = userInput.trim().replaceAll("(^-)(\\s)", "$1");
+                Log.d(TAG, "3 onTextChanged input: " + userInput);
 
-                //input=input.replaceAll("\\s+", " ");
-                //Log.d(TAG, "4 onTextChanged input: "+input);
+                userInput = userInput.replaceAll("(\\D\\s\\D)(\\s)", "$1");
+                Log.d(TAG, "5 onTextChanged input: " + userInput);
 
-                input=input.replaceAll("(\\D\\s\\D)(\\s)", "$1");
-                Log.d(TAG, "5 onTextChanged input: "+input);
-
-                input=input.replaceAll("(\\d)([-+/*])", "$1 $2");
-                Log.d(TAG, "6 onTextChanged input: "+input);
-
-                //input=input.replaceAll("\\s+", " ");
-                //Log.d(TAG, "7 onTextChanged input: "+input);
-
-                //input=input.replaceAll("(\\s)(\\.)", "$2");
-                //Log.d(TAG, "8 onTextChanged input: "+input);
+                userInput = userInput.replaceAll("(\\d)([-+/*])", "$1 $2");
+                Log.d(TAG, "6 onTextChanged input: " + userInput);
 
                 try {
-                    result = evalRPN(infixToPostfix(input));
-                    Log.d(TAG, "result: "+result);
+                    userResult = evalRPN(infixToPostfix(userInput));
+                    Log.d(TAG, "result: " + userResult);
 
-                    if (!String.valueOf(result).equals("-0.0")) {
-                        int intResult = (int)result;
-                        if (intResult==result)
-                            tv_Res.setText(String.valueOf(intResult));
-                        else {
-                            tv_Res.setText(String.valueOf(result));
-                        }
+                    if (!String.valueOf(userResult).equals("-0.0")) {
+                        int intResult = (int) userResult;
+                        resultView.setText(intResult == userResult ? String.valueOf(intResult) : String.valueOf(userResult));
                     }
+
+                } catch (NoSuchElementException e) {
+                    userResult = 0;
+                    Log.d(TAG, "onTextChanged: getting wrong input" + e);
+                    Toast.makeText(MainActivity.this, "Please check input values.", Toast.LENGTH_SHORT).show();
                 }
-                catch (NoSuchElementException ignored){}
             }
 
             public void beforeTextChanged(CharSequence c, int start, int count, int after) {
@@ -113,106 +96,121 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        tv_Res.setOnLongClickListener(v -> {
+        resultView.setOnLongClickListener(v -> {
             openDialog();
+
             return true;
         });
 
-        tv_Res.setOnClickListener(v -> Toast.makeText(this,"Удерживайте, чтобы скопировать", Toast.LENGTH_SHORT).show());
+        resultView.setOnClickListener(v -> Toast.makeText(this, "Удерживайте, чтобы скопировать", Toast.LENGTH_SHORT).show());
 
         getChildViews(findViewById(R.id.ll_nums));
 
-        for (final Button btn: buttonArrayList){
-            btn.setOnClickListener(v -> {
-                String curBtnText = btn.getText().toString();
-                addAtCursorPos(curBtnText);
+        for (final Button button : buttons) {
+            button.setOnClickListener(v -> {
+                String currentButtonText = button.getText().toString();
+                addAtCursorPos(currentButtonText);
             });
         }
 
-        Button btn_Clear = findViewById(R.id.btn_Clear);
-        btn_Clear.setOnClickListener(v -> {
-            editText.getText().clear();
-            tv_Res.setText("");
+        clearAllButton = findViewById(R.id.btn_Clear);
+        clearAllButton.setOnClickListener(v -> {
+            inputView.getText().clear();
+            resultView.setText("");
         });
 
-        Button btn_Del = findViewById(R.id.btn_Del);
-        btn_Del.setOnClickListener(v -> {
-            int length = editText.getText().length();
+        deleteOneButton = findViewById(R.id.btn_Del);
+        deleteOneButton.setOnClickListener(v -> {
+            int length = inputView.getText().length();
+
             if (length > 0) {
                 delAtCursorPos();
             }
-            if (editText.getText().length()==0)
-                tv_Res.setText("");
+
+            if (inputView.getText().length() == 0) {
+                resultView.setText("");
+            }
         });
 
-        Button btn_Dot = findViewById(R.id.btn_Dot);
-        btn_Dot.setOnClickListener(v -> {
-            int length = editText.getText().length();
-            if (length>0){
-                int index = editText.getSelectionStart();
-                if (index!=0){
-                    String lastChar = editText.getText().toString();
-                    lastChar=String.valueOf(lastChar.charAt(index-1));
-                    Log.d(TAG, "lastChar: "+lastChar);
-                    if (!(lastChar.equals(".")))
+        dotButton = findViewById(R.id.btn_Dot);
+        dotButton.setOnClickListener(v -> {
+            int length = inputView.getText().length();
+
+            if (length > 0) {
+                int index = inputView.getSelectionStart();
+
+                if (index != 0) {
+                    String lastChar = inputView.getText().toString();
+                    lastChar = String.valueOf(lastChar.charAt(index - 1));
+                    Log.d(TAG, "lastChar: " + lastChar);
+
+                    if (!(lastChar.equals("."))) {
                         addAtCursorPos(".");
+                    }
                 }
             }
         });
 
-        buttonArrayList.clear();
+        buttons.clear();
         getChildViews(findViewById(R.id.ll_funcs));
 
-        for (final Button btn: buttonArrayList){
+        for (final Button button : buttons) {
+            button.setOnClickListener(v -> {
+                int lengthET = inputView.getText().length();
+                String curBtn = button.getText().toString();
 
-            btn.setOnClickListener(v -> {
+                if (!button.getText().toString().equals("=") && lengthET != 0) {
+                    String lastChar = inputView.getText().toString();
+                    int index = inputView.getSelectionStart();
+                    lastChar = index != 0 ? String.valueOf(lastChar.charAt(index - 1)) : " ";
 
-                int lengthET = editText.getText().length();
-                String curBtn=btn.getText().toString();
+                    if (!(lastChar.equals(curBtn))) {
 
-
-                if (!btn.getText().toString().equals("=") && lengthET!=0) {
-
-                    String lastChar = editText.getText().toString();
-                    int index = editText.getSelectionStart();
-                    if(index!=0)
-                        lastChar=String.valueOf(lastChar.charAt(index-1));
-                    else lastChar=" ";
-
-                    //Log.d(TAG, "lastChar: "+lastChar);
-                    if (!(lastChar.equals(curBtn))){
                         if (lastChar.equals("/") && curBtn.equals("*")) {
-                            delAtCursorPos();
-                            addAtCursorPos(curBtn);
-                        }
-                        else if (lastChar.equals("*") && curBtn.equals("/")) {
-                            delAtCursorPos();
-                            addAtCursorPos(curBtn);
-                        }
-                        else if (lastChar.equals("+") && curBtn.equals("-")) {
-                            delAtCursorPos();
-                            addAtCursorPos(curBtn);
-                        }
-                        else if (lastChar.equals("-") && curBtn.equals("+")) {
-                            delAtCursorPos();
-                            addAtCursorPos(curBtn);
-                        }
-                        else {
+                            replaceOperator(curBtn);
+                        } else if (lastChar.equals("*") && curBtn.equals("/")) {
+                            replaceOperator(curBtn);
+                        } else if (lastChar.equals("+") && curBtn.equals("-")) {
+                            replaceOperator(curBtn);
+                        } else if (lastChar.equals("-") && curBtn.equals("+")) {
+                            replaceOperator(curBtn);
+                        } else {
                             addAtCursorPos(curBtn);
                         }
                     }
-                }
-                else if (btn.getText().toString().equals("-")){
+                } else if (button.getText().toString().equals("-")) {
                     addAtCursorPos(curBtn);
-                }
-                else if (btn.getText().toString().equals("=")) {
-                    editText.setText(tv_Res.getText());
-                    input="";
-                    editText.setSelection(editText.getText().length());
+                } else if (button.getText().toString().equals("=")) {
+                    inputView.setText(resultView.getText());
+                    userInput = "";
+                    inputView.setSelection(inputView.getText().length());
                 }
             });
         }
 
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void hideSoftwareKeyboard() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            inputView.setShowSoftInputOnFocus(false);
+        } else {
+            inputView.setOnTouchListener((v, event) -> {
+                v.onTouchEvent(event);
+                InputMethodManager inputMethod = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                if (inputMethod != null) {
+                    inputMethod.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+
+                return true;
+            });
+        }
+    }
+
+    private void replaceOperator(String curBtn) {
+        delAtCursorPos();
+        addAtCursorPos(curBtn);
     }
 
     public void getChildViews(ViewGroup viewGroup) {
@@ -221,63 +219,59 @@ public class MainActivity extends AppCompatActivity {
             int children = viewGroup.getChildCount();
             for (int i = 0; i < children; i++) {
                 View view = viewGroup.getChildAt(i);
+
                 if (view instanceof ViewGroup) {
                     getChildViews((ViewGroup) view);
                 }
+
                 if (view instanceof Button) {
                     TypedValue outValue = new TypedValue();
                     view.getContext().getTheme().resolveAttribute(android.R.attr.selectableItemBackground,
                             outValue, true);
                     view.setBackgroundResource(outValue.resourceId);
-                    buttonArrayList.add((Button) view);
+                    buttons.add((Button) view);
                 }
             }
         }
     }
 
-    private void delAtCursorPos(){
-        final EditText editText = findViewById(R.id.et_Input);
-        int index = editText.getSelectionStart();
-        if(index!=0)
-            editText.getText().delete(index - 1, index);
+    private void delAtCursorPos() {
+        int index = inputView.getSelectionStart();
+
+        if (index != 0) {
+            inputView.getText().delete(index - 1, index);
+        }
     }
 
-    private void addAtCursorPos(String curBtnText){
-        final EditText editText = findViewById(R.id.et_Input);
+    private void addAtCursorPos(String curBtnText) {
+        int lengthET = inputView.getText().length();
+        int index = inputView.getSelectionStart();
+        Log.d(TAG, "index: " + index);
 
-        int lengthET = editText.getText().length();
-        int index = editText.getSelectionStart();
-        Log.d(TAG, "index: "+index);
+        String full = inputView.getText().toString();
 
-        String full = editText.getText().toString();
+        String first = full.substring(0, index);
+        Log.d(TAG, "first: " + first);
 
-        String first = full.substring(0,index);
-        Log.d(TAG, "first: "+first);
+        String second = full.substring(index, lengthET);
+        Log.d(TAG, "second: " + second);
 
-        String second = full.substring(index,lengthET);
-        Log.d(TAG, "second: "+second);
-
-        editText.setText(String.format("%s%s%s", first, curBtnText, second));
-
-        Log.d(TAG, "lengthET: "+lengthET);
-
-        if (lengthET>0)
-            editText.setSelection(index + 1);
-        else editText.setSelection(1);
+        inputView.setText(String.format("%s%s%s", first, curBtnText, second));
+        inputView.setSelection(lengthET > 0 ? index + 1 : 1);
     }
 
-    private void openDialog(){
+    private void openDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Скопировать результат в буфер?")
                 .setCancelable(true)
                 .setPositiveButton("Да", (dialog, id) -> {
                     ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                    ClipData clip = ClipData.newPlainText("result", tv_Res.getText().toString());
+                    ClipData clip = ClipData.newPlainText("result", resultView.getText().toString());
                     clipboard.setPrimaryClip(clip);
-                    Toast.makeText(this,"Скопировано", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Скопировано", Toast.LENGTH_SHORT).show();
                 })
                 .setNegativeButton("Нет", (dialog, id) -> dialog.cancel())
-                ;
+        ;
 
         AlertDialog alert = builder.create();
         alert.show();
--- app/src/main/java/dracula/vlad/androcalc/ShuntingYard.java
+++ app/src/main/java/dracula/vlad/androcalc/ShuntingYard.java
@@ -1,98 +1,103 @@
 package dracula.vlad.androcalc;
 
 import android.util.Log;
+
 import java.util.LinkedList;
 import java.util.Stack;
 
 class ShuntingYard {
 
-    private static String TAG = "LOG_SY";
+    private static final String TAG = ShuntingYard.class.getName();
 
     static String infixToPostfix(String infix) {
 
         infix = infix.replaceAll("[^*+\\-\\d./\\s]", "");
-        //Log.d(TAG, "infixToPostfix: "+infix);
-
-        String ops = "-+/*";
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> s = new Stack<>();
+        String operators = "-+/*";
+        StringBuilder postfix = new StringBuilder();
+        Stack<Integer> priorities = new Stack<>();
 
         String token;
-        String[] array=infix.split("\\s");
+        String[] tokens = infix.split("\\s");
 
-        for (String anArray : array) {
-            token = anArray;
+        for (String singleToken : tokens) {
+            token = singleToken;
 
-            if (token.isEmpty())
+            if (token.isEmpty()) {
                 continue;
+            }
 
-            int idx = ops.indexOf(token);
+            int index = operators.indexOf(token);
 
-            // check for operator
-            if (idx != -1) {
-                if (s.isEmpty())
-                    s.push(idx);
+            if (index != -1) {
 
-                else {
-                    while (!s.isEmpty()) {
-                        int prec2 = s.peek() / 2;
-                        int prec1 = idx / 2;
-                        if (prec2 > prec1 || prec2 == prec1)
-                            sb.append(ops.charAt(s.pop())).append(' ');
-                        else break;
+                if (priorities.isEmpty()) {
+                    priorities.push(index);
+                } else {
+
+                    while (!priorities.isEmpty()) {
+                        int previousSecond = priorities.peek() / 2;
+                        int previousFirst = index / 2;
+
+                        if (previousSecond > previousFirst || previousSecond == previousFirst) {
+                            postfix.append(operators.charAt(priorities.pop())).append(' ');
+                        } else {
+                            break;
+                        }
                     }
-                    s.push(idx);
+                    priorities.push(index);
                 }
             } else {
-                sb.append(token).append(' ');
+                postfix.append(token).append(' ');
             }
         }
-        while (!s.isEmpty())
-            sb.append(ops.charAt(s.pop())).append(' ');
-        return sb.toString();
+
+        while (!priorities.isEmpty()) {
+            postfix.append(operators.charAt(priorities.pop())).append(' ');
+        }
+
+        return postfix.toString();
     }
 
-    static double evalRPN(String expr){
+    static double evalRPN(String postfix) {
 
         LinkedList<Double> stack = new LinkedList<>();
         String token;
-        String[] array= expr.split("\\s");
+        String[] array = postfix.split("\\s");
 
         for (String anArray : array) {
             token = anArray;
-            Double tokenNum = null;
+            double tokenNum;
 
             try {
                 tokenNum = Double.parseDouble(token);
-            } catch (NumberFormatException ignored) {}
-
-            if (tokenNum != null) {
-                stack.push(Double.parseDouble(token + ""));
+            } catch (NumberFormatException e) {
+                tokenNum = -0.0;
+                Log.d(TAG, "evalRPN: getting wrong number" + e);
             }
-            else if (token.equals("*")) {
+
+            if (tokenNum != -0.0) {
+                stack.push(Double.parseDouble(token + ""));
+            } else if (token.equals("*")) {
                 double secondOperand = stack.pop();
                 double firstOperand = stack.pop();
                 stack.push(firstOperand * secondOperand);
-            }
-            else if (token.equals("/")) {
+            } else if (token.equals("/")) {
                 double secondOperand = stack.pop();
                 double firstOperand = stack.pop();
                 stack.push(firstOperand / secondOperand);
-            }
-            else if (token.equals("-")) {
+            } else if (token.equals("-")) {
                 double secondOperand = stack.pop();
                 double firstOperand = stack.pop();
                 stack.push(firstOperand - secondOperand);
-            }
-            else if (token.equals("+")) {
+            } else if (token.equals("+")) {
                 double secondOperand = stack.pop();
                 double firstOperand = stack.pop();
                 stack.push(firstOperand + secondOperand);
-            }
-            else { //error
+            } else {
                 return -0.0;
             }
         }
+
         return stack.pop();
     }
 }
